c33da36e79e31d5a6de75329428886f29d49d78e
Dan <ZooDoo4U@users.noreply.github.com>
2024-10-24 07:42:53 -0700

    GlobalState
    Example of using global $state info in svelte5

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { counter } from '../shared.svelte';
+	</script>
+
+	<button onclick={() => counter.count += 1}>
+		clicks: {counter.count}
+	</button> 
+<style>
+	
+    button
+    {
+		text-align: center;    
+		height: 25px;          
+		margin-bottom: 10px;
+	} 
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Counter from '../lib/components/Counter.svelte';
+    import { counter } from '../lib/shared.svelte'
+</script>
+
+<Counter />
+<br/>
+<input bind:value={counter.name}>
+<div>{counter.name}</div>
+
+<style> 
+    input
+    {
+        border: 2px solid blue;
+        border-radius: 3px;
+        
+    }
+</style>
